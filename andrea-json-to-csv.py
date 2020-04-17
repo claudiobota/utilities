2a84c9600b083276a8b977a51fc32724c1fb1b8f
claudiobota <63822697+claudiobota@users.noreply.github.com>
2020-04-17 15:46:53 +0200

    Create andrea-json-to-csv.py

---
+++ andrea-json-to-csv.py
@@ -0,0 +1,16 @@
+import json
+import csv
+
+data_parsed = json.loads(open('data.json', 'r'))
+data = open('data.csv', 'w')
+csv_writer = csv.writer(data)
+
+count = 0
+
+for i in range(len(data_parsed)):
+    line = data_parsed[i]
+    if i == 0:
+        csv_writer.writerow(line.keys())
+    csv_writer.writerow(line.values())
+    
+data.close()
